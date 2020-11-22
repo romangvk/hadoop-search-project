8fec5451c4babca21dafe6a435e929e5d62eb249
Roman Kasparian <roman.kasparian@gmail.com>
2020-11-22 14:05:21 -0500

    added testing for gui

--- docker/src/Main.java
+++ docker/src/Main.java
@@ -1,22 +1,10 @@
 package src;
 
-import src.Actions;
 import src.LoadFiles;
-import src.SearchTerm;
-import src.TopNTerms;
 
 class Main {
   public static void main(String[] args) {
-    Actions a = new Actions();
-    a.setVisible(true);
-
     LoadFiles l = new LoadFiles();
     l.setVisible(true);
-
-    SearchTerm s = new SearchTerm();
-    s.setVisible(true);
-
-    TopNTerms t = new TopNTerms();
-    t.setVisible(true);
   }
 }
