64cafdd85bfc2c380d6dd4fb14c992a38716291e
Roman Kasparian <roman.kasparian@gmail.com>
2020-11-22 21:13:14 -0500

    added inverted indices

---
+++ gcp/src/InvertedIndices.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class InvertedIndices {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
+
+        private Text word = new Text();
+
+        private Set<String> patternsToSkip = new HashSet<String>();
+
+        private Configuration conf;
+        private BufferedReader fis;
+
+        @Override
+        public void setup(Context context) throws IOException, InterruptedException {
+            conf = context.getConfiguration();
+            if (conf.getBoolean("wordcount.skip.patterns", false)) {
+                URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+                for (URI patternsURI : patternsURIs) {
+                    Path patternsPath = new Path(patternsURI.getPath());
+                    String patternsFileName = patternsPath.getName().toString();
+                    parseSkipFile(patternsFileName);
+                }
+            }
+        }
+
+        private void parseSkipFile(String fileName) {
+            try {
+                fis = new BufferedReader(new FileReader(fileName));
+                String pattern = null;
+                while ((pattern = fis.readLine()) != null) {
+                    patternsToSkip.add(pattern);
+                }
+            } catch (IOException ioe) {
+                System.err.println(
+                        "Caught exception while parsing the cached file '" + StringUtils.stringifyException(ioe));
+            }
+        }
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+
+            String line = value.toString().toLowerCase();
+            for (String pattern : patternsToSkip) {
+                line = line.replaceAll(pattern, "");
+            }
+            StringTokenizer itr = new StringTokenizer(line);
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, new Text(fileName));
+            }
+        }
+    }
+
+    public static class FileReducer extends Reducer<Text, Text, Text, Text> {
+
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            HashMap m = new HashMap();
+            int sum = 0;
+            for (Text t : values) {
+                String str = t.toString();
+                if (m != null && m.get(str) != null) {
+                    sum = (int) m.get(str);
+                    m.put(str, ++sum);
+                } else {
+                    m.put(str, 1);
+                }
+            }
+            context.write(key, new Text(m.toString()));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+        String[] remainingArgs = optionParser.getRemainingArgs();
+        if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+            System.err.println("Usage: invertedindices <in> <out> [-skip skipPatternFile]");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "word count");
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setJarByClass(InvertedIndices.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(FileReducer.class);
+        job.setReducerClass(FileReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        List<String> otherArgs = new ArrayList<String>();
+        for (int i = 0; i < remainingArgs.length; ++i) {
+            if ("-skip".equals(remainingArgs[i])) {
+                job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+                job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+            } else {
+                otherArgs.add(remainingArgs[i]);
+            }
+        }
+        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
+        FileSystem fs = FileSystem.get(new Configuration());
+        fs.delete(new Path(otherArgs.get(1)), true);
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
